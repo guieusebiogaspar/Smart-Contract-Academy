92e01027f4560d0d1eb2325c72af3c570d649bc4
guieusebiogaspar <59066187+guieusebiogaspar@users.noreply.github.com>
2023-05-06 17:02:51 +0100

    Delete files/moodledata_data directory

--- files/moodledata_data/localcache/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-// ********** This file is generated DO NOT EDIT **********
-$CFG->siteidentifier = 'ebOvKPB4YBV9gMqw7rV6c3pNZY451hgIlocalhost';
-$CFG->bootstraphash = '5d246358791a4e200eaedb6dc008a359';
-// Only if the file is not stale and has not been defined.
-if ($CFG->bootstraphash === hash_local_config_cache() && !defined('SYSCONTEXTID')) {
-    define('SYSCONTEXTID', 1);
-}
